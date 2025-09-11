71c33f995d147cd8adacbd36fe74d6a1983192e1
Pritam Roy <roypritam3114@gmail.com>
2025-09-11 21:59:39 +0530

    front end update

--- my-portfolio/src/components/header.jsx
+++ my-portfolio/src/components/header.jsx
@@ -5,12 +5,12 @@ function Header() {
         <header className="sticky top-0 z-50 bg-slate-900 text-white shadow-md">
             <div className ="mx-auto flex max-w-5xl items-center justify-between px-6 py-4">
                <h1 className="tracking-tight font-bold text-xl">my portfolio</h1>
-               <nav className="space-x-6 hover:text-indigo-400 transition">
-                   <a href="#home" >home</a>
-                   <a href="#about">about</a>
-                   <a href="#about">projects</a>
-                   <a href="#about">skills</a>
-                   <a href="#about">contact</a>
+               <nav className="space-x-6">
+                   <a href="#home" className="hover:text-indigo-400 transition">home</a>
+                   <a href="#about" className="hover:text-indigo-400 transition">about</a>
+                   <a href="#about"className="hover:text-indigo-400 transition">projects</a>
+                   <a href="#about"className="hover:text-indigo-400 transition">skills</a>
+                   <a href="#about"className="hover:text-indigo-400 transition">contact</a>
                 </nav>
             </div>
             
