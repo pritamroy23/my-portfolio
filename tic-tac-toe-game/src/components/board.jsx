91d28c0cf145bc4e969843006872f0a0461fa1a6
Pritam Roy <roypritam3114@gmail.com>
2025-11-26 16:16:20 +0530

    logic coded successfully

--- tic-tac-toe-game/src/components/board.jsx
+++ tic-tac-toe-game/src/components/board.jsx
@@ -1,24 +1,62 @@
-import React from 'react';
+import React , {useState} from 'react';
 import Square from './square.jsx';
 const Board = () =>
 {
+    const [state, setState] =useState(Array(9).fill(null));
+    const[isxTurn, setisxTurn] =useState(true);
+
+    const checkWinner =()=> {
+        const winningCombinations = [   // All possible winning combinations
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6],
+        ]
+        for (let combination of winningCombinations) {
+            const [a, b, c] = combination;
+            if (state[a] !== null && state[a] === state[b] && state[a] === state[c]) {
+               return state[a]; // Return the winner ("X" or "O")
+            }
+
+        }
+        return null; // No winner yet
+
+    }
+    const iswinner = checkWinner();
+   
+
+    
+    const handleclick =(index) =>   
+    {
+        const copyState =[...state];
+        copyState[index] = isxTurn ? "X" : "O";
+        setState(copyState);
+        setisxTurn(!isxTurn);
+
+    }
     return (
         <div className='board-container'>
+            {iswinner ? (<>some one won </>) : (<>
             <div className='board-row'>
-                <Square />
-                <Square />
-                <Square />
+                <Square onClick={()=>handleclick(0)} value ={state[0]}/>
+                <Square onClick={()=>handleclick(1)} value ={state[1]}/>
+                <Square onClick={()=>handleclick(2)} value ={state[2]}/>
             </div>
             <div className='board-row'>
-                <Square />
-                <Square />
-                <Square />
+                <Square onClick={()=>handleclick(3)} value ={state[3]}/>
+                <Square onClick={()=>handleclick(4)} value ={state[4]}/>
+                <Square onClick={()=>handleclick(5)} value ={state[5]}/>
             </div>
             <div className='board-row'>
-                <Square />
-                <Square />
-                <Square />
+                <Square onClick={()=>handleclick(6)} value ={state[6]}/>
+                <Square onClick={()=>handleclick(7)} value ={state[7]}/>
+                <Square onClick={()=>handleclick(8)} value ={state[8]}/>
             </div>
+            </>)}
 
         </div>
     );
--- tic-tac-toe-game/src/components/square.jsx
+++ tic-tac-toe-game/src/components/square.jsx
@@ -1,10 +1,20 @@
 import React from 'react';
-const Square = () => {
+const Square = (props) => {
     return (
-        <div 
-        style ={{border:"1px solid", height:"100px", width:"100px"}}
+        <div
+        onClick={props.onClick} //whenever we r clicking on square this onClick event will be triggered
+        style ={{
+                border:"1px solid", 
+                height:"100px",
+                width:"100px", 
+                display:"flex", 
+                justifyContent:"center", 
+                alignItems:"center", 
+                fontSize:"20px", 
+                cursor:"pointer"
+            }}
         className='square'>
-            <h5>X</h5>
+            <h5>{props.value}</h5>
 
         </div>
 
